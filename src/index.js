740af1e16dcd7b70b81d9b5bba17b4399f7fc85c
Diplodocus <harry.holden.17@gmail.com>
2020-08-06 17:50:05 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import Navbar from './ui_files/navbar';
+import Tabs from './ui_files/tabs';
+import Carousel from './ui_files/carousel';
+import Tooltip from './ui_files/tooltip';
+import Contacts from './ui_files/contact';
+import './ui_files/styles/main.css';
+import './ui_files/styles/cards.css';
+
+// create navbar
+const navbar = new Navbar();
+navbar.init();
+
+// create tabs
+const tabs = new Tabs();
+tabs.init();
+
+// create about picture carousel
+const carousel = new Carousel();
+carousel.init();
+
+// create tooltips
+const tooltip = new Tooltip();
+tooltip.init();
+
+// create contacts tabs
+const contacts = new Contacts();
+contacts.init();
---
+++ src/ui_files/navbar.js
@@ -0,0 +1,97 @@
+import './styles/navbar.css';
+
+class Navbar{
+    constructor(){
+        this.navLinks = document.querySelectorAll('.navbar li');
+        this.profile = document.querySelector('.profile');
+        this.profileBack = document.querySelector('.profile-back');
+        this.home = document.querySelector('.home');
+        this.about = document.querySelector('.about');
+        this.projects = document.querySelector('.projects');
+        this.experience = document.querySelector('.experience');
+        this.contact = document.querySelector('.contact');
+    }
+    init(){
+        // create event listeners for each link and set scroll callback function
+        this.navLinks.forEach(link => {
+            // we dont want this behaviour for the img
+            if(link.classList.contains('nav')){
+                link.addEventListener('click', () => {
+                    const section = document.querySelector(link.getAttribute('data-target'));
+                    const currentPos = self.pageYOffset;
+                    const scrollOptions = {
+                        top: section.offsetTop,
+                        left: 0,
+                        behavior: 'smooth'
+                    };   
+                    window.scrollTo(scrollOptions);
+                });
+                // remove active class
+                link.classList.remove('active');
+
+                if(link.innerText === 'HOME'){
+                    link.classList.add('active');
+                }
+            }
+        });
+
+        // create event listener for scroll
+        document.addEventListener('scroll', e => {
+            if(self.pageYOffset < this.about.offsetTop){
+                this.navLinks.forEach(link => {
+                    link.classList.remove('active');
+                    if(link.getAttribute('data-target') === '.home'){
+                        link.classList.add('active');
+                    }
+                });
+            } else if(self.pageYOffset < this.projects.offsetTop && self.pageYOffset >= this.about.offsetTop){
+                this.navLinks.forEach(link => {
+                    link.classList.remove('active');
+                    if(link.getAttribute('data-target') === '.about'){
+                        link.classList.add('active');
+                    }
+                });
+            } else if(self.pageYOffset < this.experience.offsetTop && self.pageYOffset >= this.projects.offsetTop){
+                this.navLinks.forEach(link => {
+                    link.classList.remove('active');
+                    if(link.getAttribute('data-target') === '.projects'){
+                        link.classList.add('active');
+                    }
+                });
+            } else if(self.pageYOffset < this.contact.offsetTop && self.pageYOffset >= this.experience.offsetTop){
+                this.navLinks.forEach(link => {
+                    link.classList.remove('active');
+                    if(link.getAttribute('data-target') === '.experience'){
+                        link.classList.add('active');
+                    }
+                });
+            } else if(self.pageYOffset >= this.contact.offsetTop){
+                this.navLinks.forEach(link => {
+                    link.classList.remove('active');
+                    if(link.getAttribute('data-target') === '.contact'){
+                        link.classList.add('active');
+                    }
+                });
+            }
+        });
+  
+        // create click event on profile picture
+        this.profile.addEventListener('click', () => {
+            this.profileBack.classList.remove('spin');
+            this.profile.classList.remove('spin-rev');
+            this.profile.classList.add('spin');
+            this.profileBack.classList.add('spin-rev');
+        });
+        this.profileBack.addEventListener('click', () => {
+            this.profile.classList.remove('spin');
+            this.profileBack.classList.remove('spin-rev');
+            this.profileBack.classList.add('spin');
+            this.profile.classList.add('spin-rev');
+        });       
+    }
+    currentActive(){
+
+    }
+}
+
+export {Navbar as default};
