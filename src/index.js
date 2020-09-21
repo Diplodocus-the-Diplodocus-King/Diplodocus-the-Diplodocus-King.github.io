7f415ee6c4f0f9e813d937ee78dc3d8fa130bd95
Diplodocus <harry.holden.17@gmail.com>
2020-09-21 18:40:05 +0100

    testing form validation

--- src/index.js
+++ src/index.js
@@ -9,6 +9,46 @@ projects.init();
 const tooltip = new Tooltip();
 tooltip.init();
 
+// contact form validation
+function validateForm() {
+
+    // grab form input values
+    const name = document.querySelector('#name').value;
+    const email = document.querySelector('#email').value;
+    const message = document.querySelector('#message').value;
+
+    // name checks
+    if (name.length > 50){
+        alert('Name input is too long, please enter a shorthand version.');
+        return false;
+    } else if (name.match(/(<script>)/g) || name.match(/(<\/script>)/g)){
+        alert('ah ah ah what\'s the magic word?');
+        return false;
+    }
+
+    // email checks
+    if (email.length > 320){
+        alert('Email input is too long, please enter a valid email.');
+        return false;
+    } else if (email.match(/(<script>)/g) || email.match(/(<\/script>)/g)){
+        alert('ah ah ah what\'s the magic word?');
+        return false;
+    }
+
+    // message checks
+    if (message.match(/(<script>)/g) || message.match(/(<\/script>)/g)){
+        alert('ah ah ah what\'s the magic word?');
+        return false;
+    }
+}
+
+document.onload(() => {
+    const contactForm = document.querySelector('#contact-form');
+    contactForm.onsubmit = validateForm();
+    console.log(contactForm);
+});
+
+
 // Logo spin
 const navLogo = document.querySelector('.nav-logo');
 navLogo.addEventListener('click', e => {
